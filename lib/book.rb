f56eaed85c18b588c2fda5333f7c1ea4a8dd581b
sean-code <john.nganga@student.moringaschool.com>
2022-10-20 13:43:35 +0300

    def initialization done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+
+    def initialize (
+        title = "And Then There Were None")
+        @title = title
+        @author = "Agatha Christie"
+        @page_count = 272
+        @genre = "Mystery"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :title
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+
+
+    def initialize (title = "Adidas")
+        @title = title
+        @brand = 'Nike'
+        @color = 'red'
+        @size = 9.5
+        @material = 'suede'
+        @condition = "tattered"
+    end
+end
